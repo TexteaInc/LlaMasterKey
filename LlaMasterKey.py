f66b663a42067ac4136a1b0438b5a4bb1b68fd5a
Colerar <233hbj@gmail.com>
2024-01-02 23:36:19 +0800

    feat: support anyscale

--- LlaMasterKey.py
+++ LlaMasterKey.py
@@ -3,7 +3,6 @@ from typing import Optional
 from urllib.parse import urlparse
 
 import httpx
-import pyjson5
 from fastapi import FastAPI, Request, Response, status
 from starlette.background import BackgroundTask
 from starlette.responses import StreamingResponse
@@ -13,36 +12,54 @@ class Config:
     base_url: str
     openai_api_key: Optional[str] = None
     cohere_api_key: Optional[str] = None
+    anyscale_api_key: Optional[str] = None
 
     def __init__(self):
+        """
+        Loads keys for server from env vars
+        """
         self.base_url = os.environ.get("BASE_URL", default="http://127.0.0.1:8000")
         self.openai_api_key = os.environ.get("OPENAI_API_KEY")
         self.cohere_api_key = os.environ.get("CO_API_KEY")
+        self.anyscale_api_key = os.environ.get("ANYSCALE_API_KEY")
 
+    def user_env(self) -> dict[str, str]:
+        """
+        Get a dictionary of env vars for user
+        """
+        _user_env: dict[str, str] = dict()
+        if self.openai_api_key:
+            _user_env["OPENAI_BASE_URL"] = self.base_url
+            _user_env["OPENAI_API_KEY"] = "openai"
+        if self.cohere_api_key:
+            _user_env["CO_API_URL"] = self.base_url
+            _user_env["CO_API_KEY"] = "cohere"
+        if self.anyscale_api_key:
+            _user_env["ANYSCALE_BASE_URL"] = self.base_url
+            _user_env["ANYSCALE_API_KEY"] = "anyscale"
 
-def generate_env(_dict: dict[str, str]) -> str:
-    """
-    Generate a bash compatible environment exports according to string dictionary.
-    """
+        return _user_env
 
-    s = ""
-    for k, v in _dict.items():
-        s += f"export {k}=\"{v}\"\n"
+    def user_env_file(self) -> str:
+        return self.__generate_env(self.user_env())
 
-    return s
+    @staticmethod
+    def __generate_env(_dict: dict[str, str]) -> str:
+        """
+        Generate a bash compatible environment exports according to string dictionary.
+        """
+
+        s = ""
+        for k, v in _dict.items():
+            s += f"export {k}=\"{v}\"\n"
+
+        return s
 
 
 user_env: dict[str, str] = dict()
 config = Config()
-if config.openai_api_key:
-    user_env["OPENAI_BASE_URL"] = config.base_url
-    user_env["OPENAI_API_KEY"] = "openai"
-if config.cohere_api_key:
-    user_env["CO_API_URL"] = config.base_url
-    user_env["CO_API_KEY"] = "cohere"
-
 with open("generated-keys.env", "w") as f:
-    f.write(generate_env(user_env))
+    f.write(config.user_env_file())
 print("Please run bash command `source generated-keys.env` for easy key management.")
 
 app = FastAPI()
@@ -67,9 +84,11 @@ async def catch_all(request: Request, path: str, response: Response):
 
     match auth_value:
         case "openai":
-            return await __reverse_proxy(request, "https://api.openai.com/v1/", config.openai_api_key)
+            return await __reverse_proxy(request, "https://api.openai.com/v1", config.openai_api_key)
         case "cohere":
             return await __reverse_proxy(request, "https://api.cohere.ai", config.cohere_api_key)
+        case "anyscale":
+            return await __reverse_proxy(request, "https://api.endpoints.anyscale.com/v1", config.anyscale_api_key)
 
         case _:
             response.status_code = status.HTTP_400_BAD_REQUEST
