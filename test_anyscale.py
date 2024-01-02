f30ef33fb9b67d28c7990344ef416b17dd1a4577
Colerar <233hbj@gmail.com>
2024-01-02 23:36:19 +0800

    feat: support anyscale

---
+++ test_anyscale.py
@@ -0,0 +1,16 @@
+import os
+
+import openai
+
+client = openai.OpenAI(
+    base_url=os.environ["ANYSCALE_BASE_URL"],
+    api_key=os.environ["ANYSCALE_API_KEY"],
+)
+
+chat_completion = client.chat.completions.create(
+    model="meta-llama/Llama-2-7b-chat-hf",
+    messages=[{"role": "system", "content": "You are a helpful assistant."},
+              {"role": "user", "content": "Say 'Test'."}],
+    temperature=0.7
+)
+print(chat_completion.model_dump())
